bf6b8f8a10d0db62c72f1b9ea28769c2aa2ff07d
Patrick Jean Meurer <patrickjmeurer@gmail.com>
2022-08-06 19:00:37 -0300

    fix: jest problems with imports

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,9 @@ module.exports = {
   moduleNameMapper: {
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    "^@/(.*)$": "<rootDir>\\src\\$1"
+    "@/(.*)$": "<rootDir>/src/$1",
+    "@/Assets/(.*)$": "<rootDir>/src/assets/$1",
+    "@/Components/(.*)$": "<rootDir>/src/components/$1",
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
